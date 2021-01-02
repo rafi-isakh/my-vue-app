be7b49382dd74e05feeb9dfb1a39059b7f4b626e
rafi-isakh <rafiisakh@gmail.com>
2021-01-02 22:27:39 +0700

    integrate user login

--- pages/login.vue
+++ pages/login.vue
@@ -7,26 +7,26 @@
           Sign in to your account
         </h2>
       </div>
-      <form class="mt-8 space-y-6" action="#" method="POST">
+      <form class="mt-8 space-y-6" action="#" method="POST" @submit.prevent="handleSubmit">
         <input type="hidden" name="remember" value="true">
         <div class="rounded-md shadow-sm -space-y-px">
           <div>
             <label for="email-address" class="sr-only">Email address</label>
-            <input id="email-address" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address">
+            <input id="email-address" name="email" type="email" v-model="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address">
           </div>
           <div>
             <label for="password" class="sr-only">Password</label>
-            <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm my-3" placeholder="Password">
+            <input id="password" name="password" type="password" v-model="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm my-3" placeholder="Password">
           </div>
         </div>
 
         <div class="flex items-center justify-between">
-          <div class="flex items-center">
-            <input id="remember_me" name="remember_me" type="checkbox" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-2 border-gray-500 rounded">
+          <!-- <div class="flex items-center"> -->
+            <!-- <input id="remember_me" name="remember_me" type="checkbox" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-2 border-gray-500 rounded">
             <label for="remember_me" class="ml-2 block text-sm text-gray-900">
               Remember me
-            </label>
-          </div>
+            </label> -->
+          <!-- </div> -->
 
           <div class="text-sm">
             <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
@@ -46,7 +46,36 @@
             Sign in
           </button>
         </div>
+        <div>
+          <p class="text-xs text-red-500">{{errorMessage}}</p>
+        </div>
       </form>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      errorMessage: "",
+    }
+  },
+  methods: {
+    async handleSubmit() {
+      const credential = {
+        email: this.email,
+        password: this.password
+      }
+      console.log("Login " + credential)
+      let res = await this.$axios.post('/auth/login', credential).catch(err => this.printError(err))
+      console.log(res)
+    },
+    printError(error) {
+      this.errorMessage = error.response.data.message
+    }
+  }
+}
+</script>
